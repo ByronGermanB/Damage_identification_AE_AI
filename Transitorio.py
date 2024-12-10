df38f1030dc00882cf208b1e1d698b948b8782bd
byrongerman <german.barriga.m@gmail.com>
2024-12-10 17:43:17 +0100

    Translate comments and docstrings to English for better clarity

--- Transitorio.py
+++ Transitorio.py
@@ -6,45 +6,45 @@ Created on Tue Jan 30 15:31:23 2024
 """
 
 # =============================================================================
-# Importamos las librerias necesarias
+# Import necessary libraries
 # =============================================================================
-# Directorios
+# Directories
 import os
-# Funciones para extraccion de features
+# Functions for feature extraction
 from mis_funciones.analisis_AE import Features
 
 # =============================================================================
-# Datos iniciales
+# Initial data
 # =============================================================================
-# Directorio actual
+# Current directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Directorio con datos de EA
+# Directory with AE data
 EA_path = os.path.join(script_dir, 'Datos_EA')
-# Directorio para guardar imagenes
+# Directory to save images
 figure_path = os.path.join(script_dir, 'Figuras')
 os.makedirs(figure_path, exist_ok=True)
 
-# Directorio para lectura de datos EA
+# Directory for reading AE data
 test_id = 'MINA_P0_90_2'
 data = os.path.join(EA_path, test_id)  
 
-# Constantes1
-lower_freq = 0  # Limite inferior de frecuencia en kHz
-upper_freq = 900 # Limite superior de frecuencia en kHz
-sampling_rate = 5.0 # Frecuencia de adquisicion de datos por segundo en MHz
-N_samp = 1024   # Numero de muestras (datos) en cada segmento
-N_seg = 1  # Numero de segmentos (si se cambia se debe cambiar los nombres de las columnas)
-desfase = 200 # El desfase va de [0 a pretrigger*sampling rate] [0-250]
+# Constants
+lower_freq = 0  # Lower frequency limit in kHz
+upper_freq = 900 # Upper frequency limit in kHz
+sampling_rate = 5.0 # Data acquisition frequency per second in MHz
+N_samp = 1024   # Number of samples (data points) in each segment
+N_seg = 1  # Number of segments (if changed, column names must be changed)
+desfase = 200 # The offset ranges from [0 to pretrigger*sampling rate] [0-250]
 
-# Valores de filtrado
+# Filtering values
 umbral = 36
 counts = 5
 
 # =============================================================================
-# Extraccion de features
+# Feature extraction
 # =============================================================================
-# Inicializacion de clases
+# Class initialization
 trail = Features(data, lower_freq, upper_freq, sampling_rate, N_samp, N_seg, desfase, desfase_carga=0)
 trail_df = trail.feature_extr(umbral, counts, clase='[trial]', test_id=test_id)
 
